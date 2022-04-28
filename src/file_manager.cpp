028cef44341c10c168a12c4f047b7020a8a06128
estevamgalvao <estevamgalvao1@gmail.com>
2022-04-28 19:42:32 -0300

    working folders pen drive

--- src/file_manager.cpp
+++ src/file_manager.cpp
@@ -11,44 +11,23 @@ FileManager::~FileManager() {}
 
 int FileManager::GenerateFolder() {
     char commands[250];
-    char aux_command[50];
 
-    system("mkdir root");
-
-    snprintf(commands, 250, "cd root && ");
-    snprintf(commands, 250, "cd /mnt/d && ");
+    chdir("/mnt/p");
 
     /* Generating Folders */
     for (int i = 0; i < count_root_; i++)
     {   
-        snprintf(aux_command, 50, "mkdir SD%d", i);
-        strcat(commands, aux_command);
-
+        snprintf(commands, 250, "mkdir SD%d", i);
         system(commands);
         folders_++;
-        snprintf(commands, 250, "cd root && ");
-        // snprintf(commands, 250, "cd mnt/d && ");
 
         if (i == 1) {
-            snprintf(aux_command, 50, "cd SD%d && ", i);
-            strcat(commands, aux_command);
-
-            system(commands);
-
             for (int j = 0; j < count_SD1_; j++)
             {
-                snprintf(aux_command, 50, "mkdir SD%d.%d", i, j);
-                strcat(commands, aux_command);
-                
+                snprintf(commands, 250, "cd SD%d && mkdir SD%d.%d", i, i, j);
                 system(commands);
                 folders_++;
-
-                snprintf(commands, 250, "cd root && cd SD%d && ", i);
-                // snprintf(commands, 250, "cd mnt/d && cd SD%d && ", i);
             }
-            
-            snprintf(commands, 250, "cd root && ");
-            // snprintf(commands, 250, "cd mnt/d && ");
         }
     }
 
@@ -60,6 +39,8 @@ int FileManager::GenerateFiles1KB(int count_files) {
         printf("Error! Folders weren't created.");
         return 1;
     }
+
+    chdir("/mnt/p");
     
     char commands[250];
 
@@ -67,15 +48,12 @@ int FileManager::GenerateFiles1KB(int count_files) {
     {
         for (int j = 1; j <= count_files; j++)
         {
-            snprintf(commands, 250, "cd root && cd SD%d && openssl rand 1000 > file%d.txt", i, j);
+            snprintf(commands, 250, "cd SD%d && openssl rand 1000 > file%d.txt", i, j);
             /* Criando 371 arquivos de 1KB dentro de cada subdiretório em root */
             system(commands);
         }
-
     }
-
     return 0;
-
 }
 
 int FileManager::GenerateFilesSD1(int count_files) {
@@ -83,31 +61,33 @@ int FileManager::GenerateFilesSD1(int count_files) {
         printf("Error! Folders weren't created.");
         return 1;
     }
+
+    chdir("/mnt/p/SD1");
     
     char commands[250];
 
     /* Running through the SD1 subfolders except by the SD1.2 (videos only) */
     for (int i = 0; i < count_SD1_ - 1; i++)
     {
-        int times_files, times_size;
+        int files_multiplier, size_multiplier;
         switch (i)
         {
         case 0:
-            times_files = 2;
-            times_size = 4;
+            files_multiplier = 2;
+            size_multiplier = 4;
             break;
         case 1:
-            times_files = 3;
-            times_size = 8;
+            files_multiplier = 3;
+            size_multiplier = 8;
             break;
         default:
             printf("Error accessing SD1 subfolders.");
             break;
         }
-        for (int j = 1; j <= count_files*times_files; j++)
+        for (int j = 1; j <= count_files*files_multiplier; j++)
         {
             snprintf(commands, 250, 
-            "cd root && cd SD1 && cd SD1.%d && openssl rand %d000 > file%d.txt", i, times_size, j);
+            "cd SD1.%d && openssl rand %d000 > file%d.txt", i, size_multiplier, j);
             system(commands);
         }
     }
--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@ int main(int argc, char const *argv[])
     FileManager file_manager;
 
 
-    chdir("/mnt/d");
-    system("ls -la");
-    system("mkdir sotest");
-    mkdir("sotest", S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+    // chdir("/mnt/p");
+    // system("ls -la");
+    // system("mkdir sotest");
+    // mkdir("sotest", S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
 
-    // file_manager.GenerateFolder();
-    // file_manager.GenerateFiles1KB(N_FILES_ROOT);
-    // file_manager.GenerateFilesSD1(N_FILES_ROOT);
+    file_manager.GenerateFolder();
+    file_manager.GenerateFiles1KB(N_FILES_ROOT);
+    file_manager.GenerateFilesSD1(N_FILES_ROOT);
     
     return 0;
 }
